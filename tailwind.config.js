1791e9b09911f7281884175853457f288c6dfa3d
Johan Gavelan <johangavelan@gmail.com>
2021-06-06 20:52:42 -0500

    set up

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      neue: ['Neue Haas Display', 'sans-serif'],
+    },
+    colors: {
+      blue: {
+        100: '#0040BE',
+        200: '#0196FD',
+        300: '#193F8A'
+      },
+      pink: '#FF0066',
+      green: '#34CC67',
+      gold: '#E6B95C',
+      yellow: '#F0FD34',
+      aqua: '#2CE0E1',
+      dark: {
+        100: '#1A1A1A',
+        200: '#333333'
+      }
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
